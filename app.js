6aebc3b9c09090ca8bf5c74d37bddacc4f6e5094
madscodess <32851804+madscodess@users.noreply.github.com>
2023-02-25 17:53:09 +0000

    layout additions css

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ let out1 = document.getElementById('output1');
 
 console.log(input.value);
 
+// document.querySelector('#push').onclick = function(){
+//     if(document.querySelector('#taskInput input').value.length == 0){
+//         alert("Please Enter a Task")
+//     }
+
 function output() {
-    out1.innerHTML += input.value;
-    out1.innerHTML + newTask;
-    console.log("button clicked");
-}
+    document.querySelector('#tasks').innerHTML += 
+    `<div id="tasks"> 
+        <div id = "imgs-container">
+            <img id="square" src="/images/square.png">
+            <img id="tick" src="/images/right.png">
+            ${input.value}
+            </p>
+            <img id="delete" src="/images/cancel.png">
+        </div>
+        </div>
+        `;
+    
+//     out1.append(
+//         '<div><input type="text" name="text[]"/><a href="#" class="delete">delete</a></div>'); //adding new input field
+//     console.log("button clicked");
+ }
 
 addBtn.addEventListener("click", output);
