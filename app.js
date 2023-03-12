c108b1172cd5e1a02094c1358fc04e09c9591512
madscodess <32851804+madscodess@users.noreply.github.com>
2023-03-12 19:16:33 +0000

    js additions

--- app.js
+++ app.js
@@ -1,34 +1,72 @@
-let newTask = document.getElementById("tasks");
-let input = document.getElementById("taskInput");
-const addBtn = document.querySelector("#push")
-let out1 = document.getElementById('output1');
+// Selectors
+document.querySelector('form').addEventListener('submit', handleSubmitForm);
+document.querySelector('ul').addEventListener('click', handleClickDeleteOrCheck);
+document.getElementById('clearAll').addEventListener('click', handleClearAll);
 
-let deleteItem = document.getElementById('delete');
+// Event Handlers
+function handleSubmitForm(e) {
+    e.preventDefault(); //stops the default behaviour from browser on form submit 
+    //Next we’re retrieving a reference to the input element by using the querySelector
+    //We’re storing that element reference in input
+    let input = document.querySelector('input'); 
+    //we are then able to access the input value by using input.value
+    if (input.value != '')
+        addTodo(input.value);
+//reset the input value to an empty string.
+input.value = '';
+}
 
-console.log(input.value);
+function addTodo(todo) {
+    //This is the list element which is containing the output which is done for every todo element
+    let ul = document.querySelector('ul');
+    let li = document.createElement('li');
+    //The output consists of the todo text, a check button and a delete button.
+    li.innerHTML = `
+        <span class="todo-item">${todo}</span>
+        <button name="checkButton"><i class="fas fa-check-square"></i></button>
+        <!--- <button name="checkButton"> <i class="fa-regular fa-square"></i> --->
+        <button name="deleteButton" ><i class="fas fa-trash"></i></button>
+    `;
+   //new li element is added as a child to the ul element, so that it becomes visible in the browser
+    li.classList.add('todo-list-item');
+    ul.appendChild(li);
+}
 
-//add new task
-function output() {
-    document.querySelector('#tasks').innerHTML += 
-    `<div id="tasks"> 
-        <div id = "imgs-container">
-            <img id="square" src="/images/square.png">
-            <img id="tick" src="/images/right.png">
-            ${input.value}
-            </p>
-            <img id="deleteItem" src="/images/cancel.png">
-        </div>
-        </div>
-        `;
- }
+function handleClickDeleteOrCheck(e) {
+    if (e.target.name == 'checkButton')
+        checkTodo(e);
 
- //delete task
-deleteItem.addEventListener("click", function deleteTask() {
-    console.log("hello there delete task")
-} )
+    if (e.target.name == 'deleteButton')
+        deleteTodo(e);
+}
 
+//retrieve a reference to the parent node (list item), 
+//so that we’re able to apply a text decoration style of line-through to this todo.
+function checkTodo(e) {
+  //  let button = e.target.parentNode;
+    let item = e.target.parentNode;
+    if (item.style.textDecoration == 'line-through'){
+        item.style.textDecoration = 'none';
+        item.style.backgroundColor = '#FFFFFF';
+        //button.style.backgroundColor = 'none';
+        console.log("huheeib");
+    }
+    else {
+        item.style.textDecoration = 'line-through';
+        item.style.backgroundColor = '#C59595';
+    }
+        }
 
+function deleteTodo(e) {
+    let item = e.target.parentNode;
+    
+    item.addEventListener('transitionend', function () {
+        item.remove(); 
+    });
 
+    item.classList.add('todo-list-item-fall');
+}
 
-addBtn.addEventListener("click", output);
-//
+function handleClearAll(e) {
+    document.querySelector('ul').innerHTML = '';
+}
